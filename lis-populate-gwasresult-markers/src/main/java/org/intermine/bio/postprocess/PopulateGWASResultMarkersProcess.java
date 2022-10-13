c6d34ffff6873d07f778ef3b919d7674d2472572
Sam Hokin <shokin@ncgr.org>
2022-10-13 08:00:42 -0600

    Match GWAS genotypingPlatform with marker genotypingPlatform.

--- lis-populate-gwasresult-markers/src/main/java/org/intermine/bio/postprocess/PopulateGWASResultMarkersProcess.java
+++ lis-populate-gwasresult-markers/src/main/java/org/intermine/bio/postprocess/PopulateGWASResultMarkersProcess.java
@@ -22,15 +22,19 @@ import org.intermine.objectstore.ObjectStore;
 import org.intermine.objectstore.ObjectStoreException;
 import org.intermine.objectstore.ObjectStoreWriter;
 import org.intermine.objectstore.intermine.ObjectStoreInterMineImpl;
+import org.intermine.objectstore.query.ConstraintSet;
+import org.intermine.objectstore.query.ContainsConstraint;
 import org.intermine.objectstore.query.Query;
 import org.intermine.objectstore.query.QueryClass;
 import org.intermine.objectstore.query.QueryExpression;
 import org.intermine.objectstore.query.QueryField;
+import org.intermine.objectstore.query.QueryObjectReference;
 import org.intermine.objectstore.query.QueryValue;
 import org.intermine.objectstore.query.Results;
 import org.intermine.objectstore.query.ResultsRow;
 import org.intermine.objectstore.query.SimpleConstraint;
 
+import org.intermine.model.bio.GWAS;
 import org.intermine.model.bio.GWASResult;
 import org.intermine.model.bio.GeneticMarker;
 
@@ -67,11 +71,52 @@ public class PopulateGWASResultMarkersProcess extends PostProcessor {
         QueryClass qcMarker = new QueryClass(GeneticMarker.class);
         q.addFrom(qcMarker);
         q.addToSelect(qcMarker);
-	// gwasResult.markerName = marker.name
+        // 2
+        QueryClass qcGWAS = new QueryClass(GWAS.class);
+        q.addFrom(qcGWAS);
+        // constraints
+        ConstraintSet constraints = new ConstraintSet(ConstraintOp.AND);
+        // GWASResult.gwas = GWAS
+        QueryObjectReference gwasResultGWAS = new QueryObjectReference(qcGWASResult, "gwas");
+        constraints.addConstraint(new ContainsConstraint(gwasResultGWAS, ConstraintOp.CONTAINS, qcGWAS));
+	// GWASResult.markerName = GeneticMarker.name
 	QueryField gwasResultMarkerName = new QueryField(qcGWASResult, "markerName");
 	QueryField markerName = new QueryField(qcMarker, "name");
-	SimpleConstraint sc = new SimpleConstraint(gwasResultMarkerName, ConstraintOp.EQUALS, markerName);
-	q.setConstraint(sc);
+	constraints.addConstraint(new SimpleConstraint(gwasResultMarkerName, ConstraintOp.EQUALS, markerName));
+        // GWAS.genotypingPlatform = GeneticMarker.genotypingPlatform
+        QueryField gwasGenotypingPlatform = new QueryField(qcGWAS, "genotypingPlatform");
+        QueryField markerGenotypingPlatform = new QueryField(qcMarker, "genotypingPlatform");
+	constraints.addConstraint(new SimpleConstraint(markerGenotypingPlatform, ConstraintOp.EQUALS, gwasGenotypingPlatform));
+        q.setConstraint(constraints);
+
+
+	    // Query qGene = new Query();
+	    // qGene.setDistinct(false);
+	    // ConstraintSet csGene = new ConstraintSet(ConstraintOp.AND);
+	    // // 0 Gene
+	    // QueryClass qcGene = new QueryClass(Gene.class);
+	    // qGene.addFrom(qcGene);
+	    // qGene.addToSelect(qcGene);
+	    // // 1 Gene.chromosome
+	    // QueryClass qcGeneChromosome = new QueryClass(Chromosome.class);
+	    // qGene.addFrom(qcGeneChromosome);
+	    // qGene.addToSelect(qcGeneChromosome);
+	    // QueryObjectReference geneChromosome = new QueryObjectReference(qcGene, "chromosome");
+	    // csGene.addConstraint(new ContainsConstraint(geneChromosome, ConstraintOp.CONTAINS, qcGeneChromosome));
+	    // // 2 Gene.chromosomeLocation
+	    // QueryClass qcGeneLocation = new QueryClass(Location.class);
+	    // qGene.addFrom(qcGeneLocation);
+	    // qGene.addToSelect(qcGeneLocation);
+	    // QueryObjectReference geneLocation = new QueryObjectReference(qcGene, "chromosomeLocation");
+	    // csGene.addConstraint(new ContainsConstraint(geneLocation, ConstraintOp.CONTAINS, qcGeneLocation));
+	    // // require that the gene's chromosome equal that of QTLSpan
+	    // QueryValue qtlChromosomeIdValue = new QueryValue(qtlSpan.chromosomeId);
+	    // QueryField geneChromosomeIdField = new QueryField(qcGeneChromosome, "primaryIdentifier");
+	    // SimpleConstraint sameChromosomeConstraint = new SimpleConstraint(qtlChromosomeIdValue, ConstraintOp.EQUALS, geneChromosomeIdField);
+	    // csGene.addConstraint(sameChromosomeConstraint);
+
+
+
         // execute the query
         Results results = osw.getObjectStore().execute(q);
         // store GeneticMarkers in a map keyed by GWASResult.id
