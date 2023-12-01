5032bdeb5e250fe5b8e2db490b3227554824b3d2
Sam Hokin <shokin@ncgr.org>
2023-12-01 15:17:48 -0700

    Remove CDS without chromosome or supercontig reference.

--- lis-remove-orphans/src/main/java/org/intermine/bio/postprocess/RemoveOrphansProcess.java
+++ lis-remove-orphans/src/main/java/org/intermine/bio/postprocess/RemoveOrphansProcess.java
@@ -34,6 +34,7 @@ import org.intermine.objectstore.query.QueryObjectReference;
 import org.intermine.objectstore.query.Results;
 import org.intermine.objectstore.query.ResultsRow;
 import org.intermine.objectstore.query.Constraint;
+import org.intermine.model.bio.CDS;
 import org.intermine.model.bio.Gene;
 import org.intermine.model.bio.Protein;
 import org.intermine.model.bio.Transcript;
@@ -42,10 +43,10 @@ import org.apache.log4j.Logger;
 
 /**
  * Remove orphaned objects:
- *   - SequenceFeature with null chromosome and supercontig references
+ *
  *   - Gene with empty proteins collection
  *   - Protein with empty genes collection OR null transcript reference
- *   - Transcript with null protein reference
+ *   - Transcript with null protein reference or null chromosome/supercontig reference
  *
  * @author Sam Hokin
  */
@@ -65,25 +66,25 @@ public class RemoveOrphansProcess extends PostProcessor {
      */
     public void postProcess() throws ObjectStoreException {
 
-        // find SequenceFeature objects with null chromosome AND supercontig references
-        Set<SequenceFeature> orphanSequenceFeatures = new HashSet<>();
-        Query qSequenceFeature = new Query();
-        QueryClass qcSequenceFeature = new QueryClass(SequenceFeature.class);
-        qSequenceFeature.addFrom(qcSequenceFeature);
-        qSequenceFeature.addToSelect(qcSequenceFeature);
-        ConstraintSet sequenceFeatureConstraints = new ConstraintSet(ConstraintOp.AND);
-        sequenceFeatureConstraints.addConstraint(new ContainsConstraint(new QueryObjectReference(qcSequenceFeature, "chromosome"), ConstraintOp.IS_NULL));
-        sequenceFeatureConstraints.addConstraint(new ContainsConstraint(new QueryObjectReference(qcSequenceFeature, "supercontig"), ConstraintOp.IS_NULL));
-        qSequenceFeature.setConstraint(sequenceFeatureConstraints);
-        Results sequenceFeatureResults = osw.getObjectStore().execute(qSequenceFeature);
-        for (Object obj : sequenceFeatureResults.asList()) {
+        Set<CDS> orphanCDSes = new HashSet<>();
+        // find CDS objects with no chromosome reference AND no supercontig reference
+        Query qCDS = new Query();
+        QueryClass qcCDS = new QueryClass(CDS.class);
+        qCDS.addFrom(qcCDS);
+        qCDS.addToSelect(qcCDS);
+        ConstraintSet cdsConstraints2 = new ConstraintSet(ConstraintOp.AND);
+        cdsConstraints2.addConstraint(new ContainsConstraint(new QueryObjectReference(qcCDS, "chromosome"), ConstraintOp.IS_NULL));
+        cdsConstraints2.addConstraint(new ContainsConstraint(new QueryObjectReference(qcCDS, "supercontig"), ConstraintOp.IS_NULL));
+        qCDS.setConstraint(cdsConstraints2);
+        Results cdsResults2 = osw.getObjectStore().execute(qCDS);
+        for (Object obj : cdsResults2.asList()) {
             ResultsRow row = (ResultsRow) obj;
-            SequenceFeature sequenceFeature = (SequenceFeature) row.get(0);
-            orphanSequenceFeatures.add(sequenceFeature);
+            CDS cds = (CDS) row.get(0);
+            orphanCDSes.add(cds);
         }
-
-        // find Gene objects with empty proteins collection
+        
         Set<Gene> orphanGenes = new HashSet<>();
+        // find Gene objects with empty proteins collection
         Query qGene = new Query();
         QueryClass qcGene = new QueryClass(Gene.class);
         qGene.addFrom(qcGene);
@@ -96,8 +97,8 @@ public class RemoveOrphansProcess extends PostProcessor {
             orphanGenes.add(gene);
         }
 
-        // find Protein objects with empty genes collection OR no transcript reference
         Set<Protein> orphanProteins = new HashSet<>();
+        // find Protein objects with empty genes collection OR no transcript reference
         Query qProtein = new Query();
         QueryClass qcProtein = new QueryClass(Protein.class);
         qProtein.addFrom(qcProtein);
@@ -113,27 +114,42 @@ public class RemoveOrphansProcess extends PostProcessor {
             orphanProteins.add(protein);
         }
 
-        // find Transcript objects with no protein reference
         Set<Transcript> orphanTranscripts = new HashSet<>();
-        Query qTranscript = new Query();
-        QueryClass qcTranscript = new QueryClass(Transcript.class);
-        qTranscript.addFrom(qcTranscript);
-        qTranscript.addToSelect(qcTranscript);
-        qTranscript.setConstraint(new ContainsConstraint(new QueryObjectReference(qcTranscript, "protein"), ConstraintOp.IS_NULL));
-        Results transcriptResults = osw.getObjectStore().execute(qTranscript);
-        for (Object obj : transcriptResults.asList()) {
+        // Transcript 1. find Transcript objects with no protein reference
+        Query qTranscript1 = new Query();
+        QueryClass qcTranscript1 = new QueryClass(Transcript.class);
+        qTranscript1.addFrom(qcTranscript1);
+        qTranscript1.addToSelect(qcTranscript1);
+        qTranscript1.setConstraint(new ContainsConstraint(new QueryObjectReference(qcTranscript1, "protein"), ConstraintOp.IS_NULL));
+        Results transcriptResults1 = osw.getObjectStore().execute(qTranscript1);
+        for (Object obj : transcriptResults1.asList()) {
+            ResultsRow row = (ResultsRow) obj;
+            Transcript transcript = (Transcript) row.get(0);
+            orphanTranscripts.add(transcript);
+        }
+        // Transcript 2. find Transcript objects with no chromosome reference AND no supercontig reference
+        Query qTranscript2 = new Query();
+        QueryClass qcTranscript2 = new QueryClass(Transcript.class);
+        qTranscript2.addFrom(qcTranscript2);
+        qTranscript2.addToSelect(qcTranscript2);
+        ConstraintSet transcriptConstraints2 = new ConstraintSet(ConstraintOp.AND);
+        transcriptConstraints2.addConstraint(new ContainsConstraint(new QueryObjectReference(qcTranscript2, "chromosome"), ConstraintOp.IS_NULL));
+        transcriptConstraints2.addConstraint(new ContainsConstraint(new QueryObjectReference(qcTranscript2, "supercontig"), ConstraintOp.IS_NULL));
+        qTranscript2.setConstraint(transcriptConstraints2);
+        Results transcriptResults2 = osw.getObjectStore().execute(qTranscript2);
+        for (Object obj : transcriptResults2.asList()) {
             ResultsRow row = (ResultsRow) obj;
             Transcript transcript = (Transcript) row.get(0);
             orphanTranscripts.add(transcript);
         }
 
-        // remove orphaned sequence features
+        // remove orphaned CDSes
         osw.beginTransaction();
-        for (SequenceFeature sequenceFeature : orphanSequenceFeatures) {
-            osw.delete(sequenceFeature);
+        for (CDS cds : orphanCDSes) {
+            osw.delete(cds);
         }
         osw.commitTransaction();
-        LOG.info("Removed " + orphanSequenceFeatures.size() + " SequenceFeature objects with no chromosome or supercontig reference.");
+        LOG.info("Removed " + orphanCDSes.size() + " CDS objects which lack both chromosome and supercontig reference.");
         
         // remove orphaned genes
         osw.beginTransaction();
@@ -157,6 +173,6 @@ public class RemoveOrphansProcess extends PostProcessor {
             osw.delete(transcript);
         }
         osw.commitTransaction();
-        LOG.info("Removed " + orphanTranscripts.size() + " Transcript objects with no protein reference.");
+        LOG.info("Removed " + orphanTranscripts.size() + " Transcript objects with no protein reference or which lack both chromosome and supercontig reference.");
     }
 }
